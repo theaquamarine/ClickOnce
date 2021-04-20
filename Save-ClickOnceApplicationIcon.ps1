c12ebbdaf73c90156304655b0db94c6b68772233
blue <playbynumbers@gmail.com>
2021-04-20 22:36:38 +0100

    Add default destination to Save-ClickOnceApplicationIcon

--- Save-ClickOnceApplicationIcon.ps1
+++ Save-ClickOnceApplicationIcon.ps1
@@ -6,18 +6,24 @@ function Save-ClickOnceApplicationIcon {
         # The manifest to get the icon for
         [Parameter(Mandatory)]
         $Manifest,
-        # The location to save the file to
-        [Parameter(Mandatory)]
-        $Destination
+        # The location to save the file to. Defaults to %temp%.
+        $Destination = $env:TEMP
     )
+    # TODO: better default for $Destination? Temp isn't a great choice as it needs to be kept
+        # existing location for files? temp for everything?
 
     [uri]$iconFile = Get-ClickOnceApplicationIcon $Manifest
 
+    if (Test-Path -PathType -Container $Destination) {
+        # Possibly better to use product name?
+        $iconfilename = Split-Path -Leaf $iconFile
+        $Destination = Join-Path $Destination $iconfilename
+    }
+
     if ($iconFile.scheme -in 'http','https') {
-        # TODO: Check if $Destination is a container, append icon file name if so
         Invoke-WebRequest -Uri $iconFile -OutFile $Destination -ErrorAction Stop
         Write-Output $Destination
     } else {
-        Copy-Item -Path $uri.OriginalString -Destination $Destination -Force -PassThru
+        Copy-Item -Path $iconFile.OriginalString -Destination $Destination -Force -PassThru
     }
 }
