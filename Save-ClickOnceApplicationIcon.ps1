ee6b651fba9b19ce77d769afdedb8eaa6de82dc1
blue <playbynumbers@gmail.com>
2021-04-21 21:40:06 +0100

    Add specific IconFile param to Save-ClickOnceApplicationIcon

--- Save-ClickOnceApplicationIcon.ps1
+++ Save-ClickOnceApplicationIcon.ps1
@@ -1,20 +1,26 @@
 . .\Get-ClickOnceApplicationIcon.ps1
 
 function Save-ClickOnceApplicationIcon {
-    [CmdletBinding(SupportsShouldProcess)]
+    [CmdletBinding(SupportsShouldProcess, DefaultParameterSetName = 'Manifest')]
     param (
         # The manifest to get the icon for
-        [Parameter(Mandatory)]
+        [Parameter(Mandatory, ParameterSetName = 'Manifest')]
         $Manifest,
+        # A specific file to save
+        [Parameter(Mandatory, ParameterSetName = 'IconFile')]
+        [uri]$IconFile,
         # The location to save the file to. Defaults to %temp%.
         $Destination = $env:TEMP
     )
-    # TODO: IconFile parameter to use specific file rather than finding via manifest
     # TODO: better default for $Destination? Temp isn't a great choice as it needs to be kept
         # existing location for files? temp for everything?
 
-    [uri]$iconFile = Get-ClickOnceApplicationIcon $Manifest
+    if ($PSCmdlet.ParameterSetName -eq 'Manifest') {
+        [uri]$iconFile = Get-ClickOnceApplicationIcon $Manifest
+        Write-Debug "Found icon from manifest $iconFile"
+    }
 
+    # TODO: create destination dir if needed
     if (Test-Path -PathType Container $Destination) {
         # Possibly better to use product name? Icon filename's usually unique but product's probably more reliable
         $iconfilename = (Split-Path -Leaf $iconFile) -replace '\.deploy$'
